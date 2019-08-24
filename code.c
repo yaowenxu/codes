712020f3fd121de5c40bc01814da76254cc03ed7
yaowenxu <yaowen1996@hotmail.com>
2019-08-24 11:38:34 +0800

    add .gitignore file
    Signed-off-by: yaowenxu <yaowen1996@hotmail.com>

---
+++ code.c
@@ -0,0 +1,14 @@
+/**
+ * Author: Yaowen Xu
+ * Github: https://github.com/yaowenxu
+ * Organization: 北航系统结构研究所
+ * Date: 2019-08-24 11:33:55
+ * LastEditTime: 2019-08-24 11:36:17
+ * Description: code for internship
+ */
+
+#include <stdio.h>
+int main(){
+    printf("code for internship! c/c++\n");
+    return 0;
+}
