05e4437dbcfde32d4970dee617f579db801ef2ac
yaowenxu <yaowen1996@hotmail.com>
2020-03-20 16:34:34 +0800

    c++ 万能头文件测试
    Signed-off-by: yaowenxu <yaowen1996@hotmail.com>

---
+++ 牛客网/C++万能头文件测试.cc
@@ -0,0 +1,16 @@
+/**
+ * Author: Yaowen Xu
+ * Github: https://github.com/yaowenxu
+ * Organization: 北航系统结构研究所
+ * Date: 2020-03-20 16:28:21
+ * LastEditTime: 2020-03-20 16:33:44
+ * Description: 万能头文件测试
+ */
+
+#include <iostream>
+// #include<bits/stdc++.h> // 发现clang不支持, gcc是支持的；
+using namespace std;
+int main(){
+    cout << "OK!" << endl;
+}
+
